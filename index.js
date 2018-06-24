fad55abf33ff4a663f9af7544e06f120cc926cef
MiaRene15 <kids.hines@icloud.com>
2018-06-24 20:18:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function ternaryCheckCity(city) {
 function switchOnCharmFromTip(tip) {
  switch(tip) { 
   case 'generous': 
-    say = 'Thank you so much'
+    say = 'Thank you so much.'
   
  }
 }
