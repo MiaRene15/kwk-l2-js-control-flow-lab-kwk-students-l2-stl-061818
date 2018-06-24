3c757e017853ff69c33e5ed4c7879424647ef555
MiaRene15 <kids.hines@icloud.com>
2018-06-24 20:23:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your code in this file!
+
+function scuberGreetingForFeet(someValue) {
+    
+    if (someValue <= 400) {
+        return "This one is on me!"
+    } 
+    else if (someValue > 2000 && someValue < 2500) {
+        return "I will gladly take your thirty bucks."
+    }  
+    else if (someValue > 2500) {
+       return "No can do."
+    }
+    
+}
+
+function ternaryCheckCity(city) {
+  return (city === "NYC") ? "Ok, sounds good." : "No go."
+}
+
+function switchOnCharmFromTip(tip) {
+ switch(tip) { 
+  case "generous": 
+    say = "Thank you so much."; 
+    return say
+  break; 
+  case "not as generous":
+    say = "Thank you.";
+    return say 
+  break; 
+  case "thanks for everything": 
+    say = "Bye."
+    return say 
+  break;
+    }
+}
